4d991e19a14b642ee35355d8b5e31efcf03cfdf9
jw-green <repo.jwgreen@gmail.com>
2018-08-18 18:38:22 -0400

    User card contents navigation

--- frontend/src/components/UserCard.js
+++ frontend/src/components/UserCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/UserCard.css';
 
 import { connect } from 'react-redux';
@@ -7,19 +8,21 @@ class UserCard extends Component {
     render() {
         return (
             <div className="c-user_card__container">
-                <div className="c-user_card">
-                    <div className="c-user_card__info">
-                        <p className="c-user_card__username">
-                            {this.props.user_data.username}
-                        </p>
-                        <p className="c-user_card__level">
-                            {this.props.user_data.id}
-                        </p>
+                <Link to="/Contents/">
+                    <div className="c-user_card">
+                        <div className="c-user_card__info">
+                            <p className="c-user_card__username">
+                                {this.props.user_data.username}
+                            </p>
+                            <p className="c-user_card__level">
+                                {this.props.user_data.id}
+                            </p>
+                        </div>
+                        <div className="c-user_card__portrait">
+                            <img className="c-user_card__portrait" alt="portrait" src={this.props.image}/>
+                        </div>
                     </div>
-                    <div className="c-user_card__portrait">
-                        <img className="c-user_card__portrait" alt="portrait" src={this.props.image}/>
-                    </div>
-                </div>
+                </Link>
             </div>
         )
     }
